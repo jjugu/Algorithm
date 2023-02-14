17fa8f8d8d4283cc330737860a1009e27e456f7e
jjugu <124057966+jjugu@users.noreply.github.com>
2023-02-15 00:21:46 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        
+        for(int i = 0; i <= n; i++) {
+            if(i % 2 == 1) {
+                answer[i/2] = i;
+            }
+        }
+        return answer;
+    }
+}
