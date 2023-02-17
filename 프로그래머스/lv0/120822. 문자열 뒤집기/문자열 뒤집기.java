1161ea5b88613ed841e354038a2681302e55e8c7
jjugu <124057966+jjugu@users.noreply.github.com>
2023-02-17 23:19:08 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuffer sb = new StringBuffer(my_string);
+        return answer = sb.reverse().toString();
+    }
+}
