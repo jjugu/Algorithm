7214a46a283e6531a053184ee4a1597dc8769c40
규민 <124057966+jjugu@users.noreply.github.com>
2023-02-20 22:13:44 +0900

    [level 0] Title: 외계행성의 나이, Time: 10.08 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,9 +1,9 @@
 class Solution {
      public String solution(int age) {
         String answer = "";
-        String str = String.valueOf(age);                      //매개변수로 들어온 int 를 String으로 바꿈
-            for(int i = 0; i < str.length(); i++) {            //String의 크기만큼 for문
-                answer += (char) ((char) str.charAt(i) + 49);  //String의 i번째 리터럴에 49를 더한 char값을 넣음
+        String str = String.valueOf(age);
+            for(int i = 0; i < str.length(); i++) {
+                answer += (char) ((char) str.charAt(i) + 49);
             }
             return answer;
     }
