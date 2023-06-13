5f432ed0bd68148dcabaf9d6a04386bfdb40bedb
규민 <124057966+jjugu@users.noreply.github.com>
2023-06-13 11:15:12 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,23 +1,17 @@
 -- 코드를 입력하세요
-  WITH TBL_RANK
-    AS (SELECT M.MEMBER_NAME                                            AS "MEMBER_NAME"
-             , R.REVIEW_TEXT                                            AS "REVIEW_TEXT"
-             , R.REVIEW_DATE                                            AS "REVIEW_DATE"
-             , RANK() OVER (ORDER BY C.CNT DESC)                        AS "RANKNUMBER"
-          FROM MEMBER_PROFILE   M
-          JOIN REST_REVIEW      R
-            ON M.MEMBER_ID = R.MEMBER_ID 
-          JOIN (SELECT MEMBER_ID
-                     , REVIEW_ID
-                     , COUNT(REVIEW_ID) OVER (PARTITION BY MEMBER_ID)   AS "CNT"
-                  FROM REST_REVIEW
-               )                C
-            ON R.MEMBER_ID = C.MEMBER_ID 
-           AND R.REVIEW_ID = C.REVIEW_ID
+SELECT A.MEMBER_NAME
+     , B.REVIEW_TEXT
+     , DATE_FORMAT(B.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+  FROM MEMBER_PROFILE A
+  LEFT JOIN REST_REVIEW B ON A.MEMBER_ID = B.MEMBER_ID
+ WHERE A.MEMBER_ID IN 
+       (SELECT MEMBER_ID
+          FROM (SELECT MEMBER_ID
+                     , COUNT(MEMBER_ID)
+                     , DENSE_RANK () OVER (ORDER BY COUNT(MEMBER_ID) DESC) AS RK
+                 FROM REST_REVIEW
+                GROUP BY MEMBER_ID
+               ) MEMBER_PROFILE
+         WHERE RK = 1
        )
-SELECT MEMBER_NAME
-     , REVIEW_TEXT
-     , TO_CHAR(REVIEW_DATE, 'YYYY-MM-DD')
-  FROM TBL_RANK
- WHERE RANKNUMBER = 1
- ORDER BY REVIEW_DATE, REVIEW_TEXT;
+ ORDER BY 3 ASC, 2 ASC;
